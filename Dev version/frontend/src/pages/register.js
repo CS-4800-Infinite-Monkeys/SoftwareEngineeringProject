15ff45b1bec85cca1211f12abc8886d06c3dde2a
jjones <jjones@cpp.edu>
2022-04-08 09:59:51 -0700

    Added account creation frontend

--- Dev version/frontend/src/pages/register.js
+++ Dev version/frontend/src/pages/register.js
@@ -1,5 +1,45 @@
+import {useState} from "react"
+import "../index.css"
+
 const Register = () => {
-    return <><h1>Account creation page</h1></>;
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(inputs)
+    }
+    return <>
+    <h1>Create An Account</h1>
+    <form onSubmit={handleSubmit}> 
+        <label className="formLabel">First name:</label>
+        <input type="text" name="fName" value={inputs.fName || ""} onChange={handleChange}></input>            
+         <br />  
+         <br /> 
+        <label className="formLabel">Last name:</label>
+        <input type="text" name="lName" value={inputs.lName || ""} onChange={handleChange}></input>            
+        <br />  
+        <br />  
+        <label className="formLabel">Email address:</label>
+        <input type="text" name="email" value={inputs.email || ""} onChange={handleChange}></input>            
+        <br />    
+        <br />        
+        <label className="formLabel">Create password:</label>
+        <input type="password" name="password" value={inputs.password || ""} onChange={handleChange}></input>            
+        <br />
+        <br />
+        <label className="formLabel">Re-enter password:</label>
+        <input type="password" name="passwordCheck" value={inputs.passwordCheck || ""} onChange={handleChange}></input>
+        <br />
+        <br />
+        <input type="submit" value="Submit" className="buttonClass"></input>        
+    </form>
+    </>;
 };
 
 export default Register;
