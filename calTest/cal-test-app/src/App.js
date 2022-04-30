c58e153da00b354c33ddf61d34dc31b51008cd33
Tyler Yamaguchi <tmyamaguchi@cpp.edu>
2022-04-30 00:28:22 -0700

    added header and month change buttons

--- calTest/cal-test-app/src/App.js
+++ calTest/cal-test-app/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect} from 'react';
 import './App.css';
 import { getMonth } from './util'
 import CalendarHeader from './components/CalendarHeader';
 import Sidebar from './components/Sidebar';
 import Month from './components/Month';
 import { current } from 'tailwindcss/colors';
+import GlobalContext from './context/GlobalContext';
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth())
-
+  const {monthIndex} = useContext(GlobalContext)
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
   return (
     <div className="App">
       <React.Fragment>
-        <div className = "h-screen flex flex-columns">
+        <div className = "h-screen flex flex-col">
           <CalendarHeader />
           <div className="flex flex-1">
             <Sidebar />
--- calTest/cal-test-app/src/components/Sidebar.js
+++ calTest/cal-test-app/src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from "react";
+import CreateEventButton from'./CreateEventButton';
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return(
+    <aside className="border p-5 w-64">
+        <CreateEventButton/>
+    </aside>
+  )
 }
