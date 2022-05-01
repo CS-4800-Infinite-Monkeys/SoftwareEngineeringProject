1d52ed6f23dca4651a65aba0f65aa97630f3c20e
Abko11 <68087277+Abko11@users.noreply.github.com>
2022-05-01 11:32:23 -0700

    Login Functionality Fixed on Signin Page
    fixed bug for "login() is not a function"

--- Dev version/frontend/src/pages/signin.js
+++ Dev version/frontend/src/pages/signin.js
@@ -3,13 +3,47 @@ import {Outlet, Link} from "react-router-dom";
 import UserPortal from "./userportal";
 //import "../signin.css"+
 
-const SigninForm = ({Login, error}) => {
-//function SigninForm({ Login, error }) {
+const SigninForm = (/*{Login, error} <-- REMOVE THESE LATER*/) => {
     
     const [details, setDetails] = useState({email: "", password: ""});  
     
     //begin
 
+
+  //generic user for login testing
+    const defaultUser = {
+      email: "user@test.com",
+      password: "password"
+  }
+
+  const [user, setUser] = useState({email: ""});
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+      console.log(details);
+
+      if(details.email === defaultUser.email && details.password === defaultUser.password)  //here is where we incorporate database user credentials matching
+      {
+          console.log("Logged in Successfully");
+          setUser({
+              email: details.email
+          });
+          window.location.href = "/userportal"  //DO NOT USE <-- TESTING ONLY (REPLACE WITH ACTUAL ROUTING)
+
+      }
+      else
+      {
+          console.log("Credentials do not Match!");
+          setError("Credentials do not Match!");
+      }
+  }
+
+  const Logout = () => {
+      setUser({
+          email: ""
+      });
+      console.log("Logged out")
+  }
 
     //end
     const submitHandler = (e) => {
