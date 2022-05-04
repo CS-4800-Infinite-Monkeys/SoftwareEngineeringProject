d7bd346d4c1dcee468f413f930a74fabc56c6672
milotash <83149550+milotash@users.noreply.github.com>
2022-05-03 20:09:15 -0700

    Added events
    Can now create an event for different days, add title, and description and event color.

--- calTest/cal-test-app/src/components/Day.js
+++ calTest/cal-test-app/src/components/Day.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import dayjs from "dayjs";
+import GlobalContext from "../context/GlobalContext";
 
-export default function Day({day, rowIdx}) {
+export default function Day({ day, rowIdx }) {
+    const [dayEvents, setDayEvents] = useState([])
+    const { setDaySelected, setShowEventModal, savedEvents, setSelectedEvent } = useContext(GlobalContext);
+    useEffect(() => {
+        const events = savedEvents.filter(evt => dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY"));
+        setDayEvents(events)
+    }, [savedEvents, day]);
+    
+
   function getCurrentDayClass(){
     return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "bg-blue-600 text-white rounded-full w-7" : ""; 
-  }
+    }
+    
   return (
   <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -13,6 +23,21 @@ export default function Day({day, rowIdx}) {
         <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
             {day.format('DD')}
         </p>
-      </header>
+          </header>
+          <div className="flex-1 cursor-pointer"
+              onClick={() => {
+                  setDaySelected(day);
+                  setShowEventModal(true);
+              }}>
+              {dayEvents.map((evt, idx) => (
+                  <div
+                      key={idx}
+                      onClick={() => setSelectedEvent(evt)}
+                      className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+                  >
+                      {evt.title}
+                  </div>
+                  ))}
+            </div>
     </div>);
 }
--- calTest/cal-test-app/src/context/ContextWrapper.js
+++ calTest/cal-test-app/src/context/ContextWrapper.js
@@ -1,11 +1,39 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useReducer} from "react";
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
+import { values } from "lodash";
+
+function savedEventsReducer(state, { type, payload }) {
+    switch (type) {
+        case "push":
+            return [...state, payload];
+        case "update":
+            return state.map(evt => evt.id === payload.id ? payload : evt);
+        case "delete":
+            return state.filter(evt => evt.id !== payload.id);
+        default:
+            throw new Error();
+    }
+}
+
+function initEvents() {
+    const storageEvents = localStorage.getItem("savedEvents")
+    const parsedEvents = storageEvents ? JSON.parse(storageEvents) : []
+    return parsedEvents
+}
 
 export default function ContextWrapper(props) {
     const[monthIndex, setMonthIndex] = useState(dayjs().month());
     const[smallCalendarMonth, setSmallCalendarMonth] = useState(null);
-    const[daySelected, setDaySelected] = useState(null);
+    const [daySelected, setDaySelected] = useState(dayjs());
+    const [showEventModal, setShowEventModal] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [savedEvents, dispatchCalEvent] = useReducer(savedEventsReducer, [], initEvents);
+
+    useEffect(() => {
+        localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+    }, [savedEvents])
+
     useEffect(() => {
         if(smallCalendarMonth !== null){
             setMonthIndex(smallCalendarMonth)
@@ -18,7 +46,13 @@ export default function ContextWrapper(props) {
             setSmallCalendarMonth, 
             smallCalendarMonth,
             daySelected,
-            setDaySelected
+          setDaySelected,
+          showEventModal,
+          setShowEventModal,
+          dispatchCalEvent,
+          selectedEvent,
+          setSelectedEvent,
+            savedEvents
             }}>
           {props.children}
       </GlobalContext.Provider>
